cc657f6f5f3054f68fa7a199abfeed56832b6469
Hairum Qureshi <159098695+Hairum-Qureshi@users.noreply.github.com>
2026-02-27 09:18:07 -0500

    added scroll.h with display utility function declarations

---
+++ scroll.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void openDisplay(void);
+
+void closeDisplay(void);
+
+void displayLetter(char letter, int xOffset, int yOffset);
+
+void clearDisplay(void);
+
+int checkInput(int delay);
+
+#endif 
